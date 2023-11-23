e8d1e4fc2daed687120c0599e30ca89fcd4407b2
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 06:41:00 +0000

    yes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,4 @@
-
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { useSession } from 'next-auth/react';
 import Layout from '@/components/layout/layout';
 import '@/app/globals.css';
@@ -8,18 +7,20 @@ export default function Dashboard() {
   const { data: session, status } = useSession();
 
   if (status === 'loading') {
-    return <div>Loading...</div>; // Or any loading indicator
+    return <div>Loading...</div>;
   }
 
   if (!session) {
-    // Redirect to login or show an error message
     return <div>Access Denied. Please log in.</div>;
   }
+
+  // Use optional chaining (?.) to safely access user and name
+  const userName = session.user?.name || 'User'; // Provide a default value
 
   return (
     <Layout>
       <div className="dashboard-container">
-        <h1>Welcome to Your Dashboard, {session.user.name}</h1>
+        <h1>Welcome to Your Dashboard, {userName}</h1>
         {/* Dashboard content goes here */}
       </div>
     </Layout>

fdea4adf3b60af8d79e360025d12df7cf6446e5e
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 06:37:35 +0000

    yes

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,18 @@
 import React, { ReactNode } from 'react';
-import Navbar from './navbar'; // Adjust the path as necessary
-import Footer from './footer'; // Adjust the path as necessary
+import { useSession } from 'next-auth/react'; // Import the useSession hook
+import Navbar from './navbar';
+import Footer from './footer';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const { data: session } = useSession(); // Get the session data
+
   return (
     <>
-      <Navbar />
+      <Navbar session={session} /> {/* Pass the session to Navbar */}
       <main>{children}</main>
       <Footer />
     </>

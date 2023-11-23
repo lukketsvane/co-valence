06ab6874acb21af157ac9890483eafeac021e53b
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 01:36:14 +0000

    ye

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,35 +4,32 @@ import cx from "classnames";
 import { sfPro, inter } from "./fonts";
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
-import Head from "next/head";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Co:valence - Building blocks for your Next.js project",
   description:
     "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  metadataBase: new URL("https://co-valence.dev"),
+  metadataBase: new URL("https://precedent.dev"),
+  themeColor: "#FFF",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <Nav />
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+          <Nav />
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
           {children}
         </main>
         <Footer />
         <Analytics />
       </body>
-    </>
+    </html>
   );
 }

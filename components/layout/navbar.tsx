1e379128e456e84ff1a1abb4bdcbe9e3060b3538
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 02:52:56 +0000

    ye

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,34 +1,44 @@
-
 "use client";
 import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import { SessionProvider, useSession } from "next-auth/react";
 import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
-
-
-
-import { Session } from "next-auth";
 import { Moon, Sun } from 'lucide-react';
 
-export default function NavBar({ session }: { session: Session | null }) {
+export default function NavBarWrapper() {
+  return (
+    <SessionProvider session={null}>
+      <NavBar />
+    </SessionProvider>
+  );
+}
+
+function NavBar() {
   const { SignInModal, setShowSignInModal } = useSignInModal();
+  const { data: session, status } = useSession();
   const scrolled = useScroll(50);
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    const savedMode = localStorage.getItem('dark-mode') === 'true';
+    // Get the theme from localStorage and apply it
+    const savedMode = localStorage.getItem('theme') === 'dark';
     setDarkMode(savedMode);
-    document.body.classList.toggle('dark', savedMode);
+    document.documentElement.setAttribute('data-theme', savedMode ? 'dark' : 'light');
   }, []);
 
   const toggleDarkMode = () => {
     const newMode = !darkMode;
-    localStorage.setItem('dark-mode', newMode.toString());
-    document.body.classList.toggle('dark', newMode);
+    localStorage.setItem('theme', newMode ? 'dark' : 'light');
+    document.documentElement.setAttribute('data-theme', newMode ? 'dark' : 'light');
     setDarkMode(newMode);
   };
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -41,15 +51,17 @@ export default function NavBar({ session }: { session: Session | null }) {
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-            <p>co:valence</p>
+          <Link href="/" passHref>
+            <div className="flex items-center font-display text-2xl">
+              <Image
+                src="/logo.png"
+                alt="Precedent logo"
+                width={30}
+                height={30}
+                className="mr-2 rounded-sm"
+              />
+              <span>co:valence</span>
+            </div>
           </Link>
           <div className="flex items-center">
             <button
---
+++ lib/themeContext.tsx
@@ -0,0 +1,43 @@
+// lib/ThemeContext.tsx
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme') as Theme;
+    setTheme(storedTheme || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+}

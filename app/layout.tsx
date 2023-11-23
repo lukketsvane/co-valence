81202daefce294deab16bcd6fa3003b136cab826
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 03:17:55 +0000

    ye

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,36 @@
-"use client";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import cx from "classnames";
 import { sfPro, inter } from "./fonts";
-import Nav from "@/components/layout/nav";
+import Nav from "@/components/layout/nav"; // This is a client component
 import Footer from "@/components/layout/footer";
-import React, { ReactNode, useEffect } from "react";
-import { useDarkMode } from "@/lib/hooks/useDarkMode";
+import React, { ReactNode } from "react";
 
-
-
+export const metadata = {
+  title: "Co:valence - Building blocks for your Next.js project",
+  description: "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+  metadataBase: new URL("https://precedent.dev"),
+  themeColor: "#FFF",
+};
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const [darkMode, toggleDarkMode] = useDarkMode();
-
-  const metadata = {
-    title: "Co:valence - Building blocks for your Next.js project",
-    description: "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    metadataBase: new URL("https://precedent.dev"),
-    themeColor: "#FFF",
-  };
-
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', darkMode);
-  }, [darkMode]);
-
   return (
-    <div className={cx(sfPro.variable, inter.variable, darkMode ? 'dark' : '')}>
-      <button onClick={toggleDarkMode}>
-        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-      </button>
-
-      <Nav />
-      <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-        {children}
-      </main>
-      <Footer />
-      <Analytics />
-    </div>
+    <>
+      <head>
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Nav /> {/* Nav component will handle the dark mode */}
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          {children}
+        </main>
+        <Footer />
+        <Analytics />
+      </body>
+      </head>
+      </>
   );
 }
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,43 +1,15 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+
 import Image from "next/image";
 import Link from "next/link";
-import { SessionProvider, useSession } from "next-auth/react";
 import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
-import { Moon, Sun } from 'lucide-react';
+import { Session } from "next-auth";
 
-export default function NavBarWrapper() {
-  return (
-    <SessionProvider session={null}>
-      <NavBar />
-    </SessionProvider>
-  );
-}
-
-function NavBar() {
+export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
-  const { data: session, status } = useSession();
   const scrolled = useScroll(50);
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedMode = localStorage.getItem('theme') === 'dark';
-    setDarkMode(savedMode);
-    document.documentElement.setAttribute('data-theme', savedMode ? 'dark' : 'light');
-  }, []);
-
-  const toggleDarkMode = () => {
-    const newMode = !darkMode;
-    localStorage.setItem('theme', newMode ? 'dark' : 'light');
-    document.documentElement.setAttribute('data-theme', newMode ? 'dark' : 'light');
-    setDarkMode(newMode);
-  };
-
-  if (status === 'loading') {
-    return <div>Loading...</div>;
-  }
 
   return (
     <>
@@ -45,35 +17,27 @@ function NavBar() {
       <div
         className={`fixed top-0 w-full flex justify-center ${
           scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl dark:border-gray-700 dark:bg-gray-800/50"
-            : "bg-white/0 dark:bg-gray-800/0"
+            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+            : "bg-white/0"
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" passHref>
-            <div className="flex items-center font-display text-2xl">
-              <Image
-                src="/logo.png"
-                alt="Precedent logo"
-                width={30}
-                height={30}
-                className="mr-2 rounded-sm"
-              />
-              <span>co:valence</span>
-            </div>
+          <Link href="/" className="flex items-center font-display text-2xl">
+            <Image
+              src="/logo.png"
+              alt="Precedent logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
+            <p>co:valance</p>
           </Link>
-          <div className="flex items-center">
-            <button
-              onClick={toggleDarkMode}
-              className="mr-4 p-2 text-xl text-gray-800 dark:text-gray-200"
-            >
-              {darkMode ? <Sun /> : <Moon />}
-            </button>
+          <div>
             {session ? (
               <UserDropdown session={session} />
             ) : (
               <button
-                className="rounded-full border border-black dark:border-white bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white"
+                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
                 onClick={() => setShowSignInModal(true)}
               >
                 Sign In

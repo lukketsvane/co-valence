882d8a3ead4209e0a2cb31619f38d3224f805378
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 08:03:16 +0000

    ye

--- components/dashboard/chatbox.tsx
+++ components/dashboard/chatbox.tsx
@@ -21,25 +21,46 @@ export default function ChatBox({
   };
 
   return (
-    <div className="relative col-span-1 overflow-hidden rounded-lg border border-gray-300 bg-white shadow-sm">
-      <div className="border-b border-gray-200 p-6">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
-          <div className="flex space-x-2">
+    <div className="relative max-w-2xl mx-auto my-6 overflow-hidden rounded-lg border border-gray-200 bg-white shadow">
+      <div className="flex justify-between items-center p-4 bg-gray-50 border-b border-gray-200">
+        <div>
+          <h2 className="text-lg font-semibold">{title}</h2>
+          <p className="text-sm text-gray-600">Industry: Oil & Gas Industry | Application: Offshore Drilling Operations</p>
+        </div>
+        <div className="flex space-x-2">
             <button className="p-2 rounded hover:bg-gray-100">
               <Image src="/upload.png" alt="Upload data" width={20} height={20} />
             </button>
             <button className="p-2 rounded hover:bg-gray-100">
               <Image src="/list.png" alt="List concepts" width={20} height={20} />
             </button>
-          </div>
-        </div>
-        <div className="h-60 overflow-y-auto mb-4 p-4 bg-gray-50">
-          {/* ... */}
         </div>
       </div>
-      <div className="flex items-center p-4">
-        {/* ... */}
+      <div className="p-4">
+        <div className="h-48 overflow-y-auto mb-4">
+          {messages.length > 0 ? (
+            messages.map((message, index) => (
+              <div key={index} className="p-2 bg-white rounded-lg shadow mb-2">
+                {message}
+              </div>
+            ))
+          ) : (
+            <div className="text-gray-400 italic">No messages...</div>
+          )}
+        </div>
+        <textarea
+          rows={4}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50"
+          placeholder={placeholder}
+        />
+        <button
+          onClick={handleSendMessage}
+          className="mt-2 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded-md"
+        >
+          Send
+        </button>
       </div>
     </div>
   );

2aaa89152a858cb8d82d43760f65c3c0b024047e
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 01:19:45 +0000

    ye

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -14,17 +14,23 @@ export default function NavBar({ session }: { session: Session | null }) {
   return (
     <>
       <SignInModal />
-      <div>
+      <div
+        className={`fixed top-0 w-full flex justify-center ${
+          scrolled
+            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+            : "bg-white/0"
+        } z-30 transition-all`}
+      >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
-              alt="Co:valence logo"
+              alt="Precedent logo"
               width="30"
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>Co:valence</p>
+            <p>co:valance</p>
           </Link>
           <div>
             {session ? (

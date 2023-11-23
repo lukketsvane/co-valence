484ec6c78c3c9edf086103f3d5555b93d3c5d7d0
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 00:38:58 +0000

    ye

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
+  title: "Co:valence - Building blocks for your Next.js project",
   description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+    "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
   metadataBase: new URL("https://precedent.dev"),
   themeColor: "#FFF",
 };
@@ -23,9 +23,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={cx(sfPro.variable, inter.variable)}>
         <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Suspense fallback="...">
           <Nav />
-        </Suspense>
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
           {children}
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -11,35 +11,33 @@ export default function Home() {
   const { data, error } = useSWR("https://api.github.com/repos/lukketsvane/co-valence", fetcher);
   const stars = data ? data.stargazers_count : 0;
 
-
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
       className="dark:bg-gray-900"
     >
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <motion.h1
           initial={{ y: -100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.2 }}
-          className="bg-gradient-to-br from-green-300 to-blue-500 bg-clip-text text-center font-display text-4xl font-bold text-transparent md:text-7xl"
+          transition={{ delay: 0.2, duration: 1 }}
+          className="bg-gradient-to-br from-green-300 to-blue-500 bg-clip-text text-center font-display text-4xl font-bold md:text-7xl"
         >
           co:valence
         </motion.h1>
         <motion.p
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ delay: 0.4 }}
+          transition={{ delay: 0.4, duration: 1 }}
           className="mt-6 text-center text-gray-500 md:text-xl dark:text-gray-300"
         >
           A modern toolkit for psychological health professionals.
         </motion.p>
-        {/* Additional content and components */}
+
+
       </div>
       <div className="my-10 grid w-full max-w-screen-xl grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-        {/* Map over the features to create cards for each one */}
         {features.map(({ title, description, demo, large }) => (
           <Card
             key={title}
@@ -58,32 +56,32 @@ const features = [
   {
     title: "Interactive Chatbot",
     description: "Engage with patients using a smart AI-driven chatbot that helps in preliminary diagnosis and common inquiries.",
-    demo: <ComponentGrid />, // Replace with the actual component that demonstrates the chatbot feature.
+    demo: <ComponentGrid />, 
     large: true,
   },
   {
     title: "Patient Journaling System",
     description: "Maintain patient records with ease using a secure and intuitive journaling system, accessible from anywhere.",
-    demo: <Image src="/journal-demo.png" alt="Journal Demo" width={300} height={200} />, // Replace with the actual image or component.
+    demo: <Image src="/journal-demo.png" alt="Journal Demo" width={300} height={200} />, 
   },
   {
     title: "Data-Driven Insights",
     description: "Gain valuable insights into patient trends with data analytics tools, helping you make informed decisions.",
-    demo: <Image src="/data-insights-demo.png" alt="Data Insights Demo" width={300} height={200} />, // Replace with the actual image or component.
+    demo: <Image src="/data-insights-demo.png" alt="Data Insights Demo" width={300} height={200} />,
   },
   {
     title: "Appointment Scheduling",
     description: "Streamline your appointment booking process with an integrated scheduling tool that syncs with your calendar.",
-    demo: <Image src="/scheduling-demo.png" alt="Scheduling Demo" width={300} height={200} />, // Replace with the actual image or component.
+    demo: <Image src="/scheduling-demo.png" alt="Scheduling Demo" width={300} height={200} />, 
   },
   {
     title: "Resource Library",
     description: "Access a wealth of mental health resources, articles, and research papers to support your practice.",
-    demo: <Image src="/resources-demo.png" alt="Resources Demo" width={300} height={200} />, // Replace with the actual image or component.
+    demo: <Image src="/resources-demo.png" alt="Resources Demo" width={300} height={200} />, 
   },
   {
     title: "Telehealth Ready",
     description: "Conduct remote consultations with a built-in telehealth feature that offers both video and messaging capabilities.",
-    demo: <Image src="/telehealth-demo.png" alt="Telehealth Demo" width={300} height={200} />, // Replace with the actual image or component.
+    demo: <Image src="/telehealth-demo.png" alt="Telehealth Demo" width={300} height={200} />, 
   }
 ];

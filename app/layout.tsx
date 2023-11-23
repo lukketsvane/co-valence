71f37cf9318c015f73fb7b6d37a963270dc7a89c
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 03:14:53 +0000

    ye

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ import Footer from "@/components/layout/footer";
 import React, { ReactNode, useEffect } from "react";
 import { useDarkMode } from "@/lib/hooks/useDarkMode";
 
+
+
+
 type RootLayoutProps = {
   children: ReactNode;
 };
@@ -15,7 +18,6 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   const [darkMode, toggleDarkMode] = useDarkMode();
 
-  // Ensure metadata is updated to reflect latest changes or loaded from an external source if needed
   const metadata = {
     title: "Co:valence - Building blocks for your Next.js project",
     description: "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
@@ -24,18 +26,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
   };
 
   useEffect(() => {
-    // Toggle the dark mode class on the root element
     document.documentElement.classList.toggle('dark', darkMode);
   }, [darkMode]);
 
   return (
     <div className={cx(sfPro.variable, inter.variable, darkMode ? 'dark' : '')}>
+      <button onClick={toggleDarkMode}>
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
 
       <Nav />
       <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
         {children}
       </main>
-
       <Footer />
       <Analytics />
     </div>

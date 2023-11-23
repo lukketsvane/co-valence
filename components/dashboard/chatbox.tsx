7c90e258d94f9807dad5e53f292894e985327bfa
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 08:22:30 +0000

    ye

--- components/dashboard/chatbox.tsx
+++ components/dashboard/chatbox.tsx
@@ -1,46 +1,45 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from 'next/image';
+import { FiSend } from 'react-icons/fi';
+
 
 export default function ChatBox({
   title = "Describe a study",
   initialMessages = [],
-  placeholder = "Company X operates an offshore drilling platform in the North Sea, \"Area Y\". This platform features advanced drilling machinery, safety systems, and environmental monitoring equipment. Key operations include power generation, drilling control, and crew safety. The scenario involves routine maintenance, repairs, and upgrades to improve efficiency and safety, focusing on machinery, power systems, and emergency response enhancements."
 }: {
   title?: string;
   initialMessages?: string[];
-  placeholder?: string;
 }) {
   const [messages, setMessages] = useState<string[]>(initialMessages);
-  const [newMessage, setNewMessage] = useState("");
+  const messageInputRef = useRef<HTMLDivElement>(null);
 
   const handleSendMessage = () => {
-    if (newMessage.trim() !== "") {
-      setMessages([...messages, newMessage]);
-      setNewMessage("");
+    if (messageInputRef.current && messageInputRef.current.innerText.trim() !== "") {
+      setMessages([...messages, messageInputRef.current.innerText]);
+      messageInputRef.current.innerText = ""; // Clear the contenteditable div
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSendMessage();
     }
   };
 
   return (
     <div className="relative max-w-2xl mx-auto my-6 overflow-hidden rounded-lg border border-gray-200 bg-white shadow">
       <div className="flex justify-between items-center p-4 bg-gray-50 border-b border-gray-200">
-        <div>
+      <div>
           <h2 className="text-lg font-semibold">{title}</h2>
           <p className="text-sm text-gray-600">Industry: Oil & Gas Industry | Application: Offshore Drilling Operations</p>
         </div>
-        <div className="flex space-x-2">
-            <button className="p-2 rounded hover:bg-gray-100">
-              <Image src="/upload.png" alt="Upload data" width={20} height={20} />
-            </button>
-            <button className="p-2 rounded hover:bg-gray-100">
-              <Image src="/list.png" alt="List concepts" width={20} height={20} />
-            </button>
-        </div>
       </div>
-      <div className="p-4">
-        <div className="h-48 overflow-y-auto mb-4">
+      <div className="p-4 pr-16 ">
+        <div className="h-auto overflow-y-auto mb-4">
           {messages.length > 0 ? (
             messages.map((message, index) => (
-              <div key={index} className="p-2 bg-white rounded-lg shadow mb-2">
+              <div key={index} className="p-2 pr-6 bg-white rounded-lg shadow mb-2">
                 {message}
               </div>
             ))
@@ -48,19 +47,25 @@ export default function ChatBox({
             <div className="text-gray-400 italic">No messages...</div>
           )}
         </div>
-        <textarea
-          rows={4}
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50"
-          placeholder={placeholder}
-        />
-        <button
-          onClick={handleSendMessage}
-          className="mt-2 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded-md"
-        >
-          Send
-        </button>
+        <div
+          ref={messageInputRef}
+          onKeyPress={handleKeyPress}
+          contentEditable
+          className="w-full mt-2 p-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50"
+          role="textbox"
+          tabIndex={0} // to make div focusable
+          style={{ minHeight: '50px' }} // Set a minimum height
+        ></div>
+          <div className="absolute bottom-3 right-4 p-auto">
+            <button
+              style={{ minHeight: '50px' }} // Set a minimum height
+              onClick={handleSendMessage}
+              aria-label="Send message"
+              className="text-black hover:text-blue-600"
+            >
+              <FiSend className="mr-2" size={26} />
+            </button>
+          </div>
       </div>
     </div>
   );

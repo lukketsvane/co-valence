228324a59aedac233d0a8cb3d33cae88178f2ce7
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 03:13:16 +0000

    ye

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,20 +15,27 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   const [darkMode, toggleDarkMode] = useDarkMode();
 
+  // Ensure metadata is updated to reflect latest changes or loaded from an external source if needed
+  const metadata = {
+    title: "Co:valence - Building blocks for your Next.js project",
+    description: "Co:valence is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+    metadataBase: new URL("https://precedent.dev"),
+    themeColor: "#FFF",
+  };
+
   useEffect(() => {
+    // Toggle the dark mode class on the root element
     document.documentElement.classList.toggle('dark', darkMode);
   }, [darkMode]);
 
   return (
-    <div className={cx(sfPro.variable, inter.variable, { 'dark': darkMode })}>
-      <button onClick={toggleDarkMode}>
-        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-      </button>
+    <div className={cx(sfPro.variable, inter.variable, darkMode ? 'dark' : '')}>
 
       <Nav />
       <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
         {children}
       </main>
+
       <Footer />
       <Analytics />
     </div>

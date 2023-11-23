53b5843b65960e9bec2473f3b3256aed08bc7a35
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 10:51:25 +0000

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ export default async function Home() {
 
   return (
     <>
-      <div className="z-10 h-fit w-full max-w-xl px-5 xl:px-0">
+      <div className="z-10 py-12 w-full max-w-2xl px-5 xl:px-0">
         <h1
-          className="py-10 animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-6xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-9xl md:leading-[5rem]"
+          className="py-12 animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-6xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-9xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           co:valence

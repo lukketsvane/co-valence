6928125194354e33e2c16c3846c95a6b465790d8
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 06:38:47 +0000

    yes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
+import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '@/app/globals.css';
 
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />

6ec8cc1a5f368a257b935a045f7dfb7054c51063
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 12:03:48 +0000

    ye

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,14 @@
-// dashboard.tsx
-
 import React, { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import Layout from '@/components/layout/layout';
 import ChatBox from '@/components/dashboard/chatbox';
-import Sidebar, { Agent } from '@/components/dashboard/sidebar'; // Import the Sidebar component and Agent interface
+import Sidebar, { Agent } from '@/components/dashboard/sidebar'; // Ensure to import the Agent type
 import '@/app/globals.css';
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
   const [systemMessage, setSystemMessage] = useState('');
+  const [selectedAgentName, setSelectedAgentName] = useState('Wordle'); // Default to 'Wordle' or the first agent
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -21,14 +20,18 @@ export default function Dashboard() {
 
   const userName = session.user?.name || 'User';
 
-  const handleAgentSelect = (agent: Agent) => { // Use the Agent type here
+  // Add the Agent type to the agent parameter
+  const handleAgentSelect = (agent: Agent) => {
     setSystemMessage(agent.systemMessage);
+    setSelectedAgentName(agent.name); // Update the selected agent name
   };
+
 
   return (
     <Layout>
       <div className="flex">
-        <Sidebar onAgentSelect={handleAgentSelect} />
+        {/* Pass the selectedAgentName to the Sidebar */}
+        <Sidebar onAgentSelect={handleAgentSelect} selectedAgentName={selectedAgentName} />
         <main className="main-content flex-1">
           <ChatBox title={`Welcome to Your Dashboard, ${userName}`} systemMessage={systemMessage} initialMessages={[]} />
         </main>

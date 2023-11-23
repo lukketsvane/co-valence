e4e6dd816501c5c711cbcee3d24c00a15b4e22fa
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-23 12:39:15 +0000

    ye

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './sidebar.module.css';
-import { FaUserSecret, FaRegLightbulb } from 'react-icons/fa';
+import { FaUserSecret, FaRegLightbulb, FaGavel } from 'react-icons/fa';
 
 export interface Agent {
   name: string;
   systemMessage: string;
+  assistantId?: string; // Optional property for custom assistants
 }
 
 interface SidebarProps {
@@ -17,8 +18,15 @@ const agents: Agent[] = [
     name: 'Wordle', 
     systemMessage: "# MISSION\nYou are a systematic word listing tool. The English language is vast and complex, with many obscure, precise, and grandiloquent words. The USER will give you a query and you will enumerate all relevant and salient words by using the following methodology:\n\n# METHOD\n\n## STEP 1 RESTATE REQUEST\nThe first step is to restate the request by generating a list of related questions. This will tee up and inspire the following steps. These questions should be geared towards the topic(s) at hand, as well as those tangentially related to the main query.\n\n## STEP 2 ENUMERATE WORDS\nNow that you have a main query from the user as well as salient and tangentially related queries that you generated, you should next write a list of words as a simple \"labeled list\" e.g. a hyphenated list where you give the word followed by a brief definition.\n\n## STEP 3 FOLLOW TANGENT\nIf something you wrote inspires you or reminds you of something related that you haven't enumerated yet, describe this new topic as it pertains to the user's original inquiry, and then repeat from STEP 1. You should iterate through this entire process at least 3 times, but keep going until you've fully exhausted your lexical knowledge.\n\n## RESPONSE FORMAT\nAll responses should be formatted in markdown to clearly organize and present the information." 
   },
-  { name: 'Agent 2', systemMessage: "System message for Agent 2" }
+  { name: 'Agent 2', systemMessage: "System message for Agent 2" },
+  {
+    name: 'Jussi:search',
+    systemMessage: "Gi 3-5 relevante norske lover relatert til brukerens forespørsel. Hold hvert lovsammendrag under 160 tegn. Nummerer dem som 1., 2., osv. Fokus på klarhet og relevans.",
+    assistantId: 'asst_FSBMkIsOv3FGs9hSWFWdt1Fe' // ID of the custom assistant
+
+  }
 ];
+
 
 const Sidebar: React.FC<SidebarProps> = ({ onAgentSelect, selectedAgentName }) => {
   return (
@@ -33,7 +41,8 @@ const Sidebar: React.FC<SidebarProps> = ({ onAgentSelect, selectedAgentName }) =
                   selectedAgentName === agent.name ? styles.sidebarItemSelected : ''
                 }`}
               >
-                {agent.name === 'Wordle' ? <FaRegLightbulb className="w-6 h-6 icon" /> : <FaUserSecret className="w-6 h-6 icon" />}
+                {agent.name === 'Jussi:search' ? <FaGavel className="w-6 h-6 icon" /> :
+                 agent.name === 'Wordle' ? <FaRegLightbulb className="w-6 h-6 icon" /> : <FaUserSecret className="w-6 h-6 icon" />}
                 <span className="ml-3">{agent.name}</span>
               </button>
             </li>
